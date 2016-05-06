049524264c3e94d9bf7b19a23d6616962eb193bd
un.def <un.def@ya.ru>
2016-05-06 23:31:59 +0300

    argparse initial

--- lip.py
+++ lip.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # lip v0.0.4
-import sys
-import socket
-import re
-import subprocess
-import os
-import signal
 
-py3 = True if sys.version_info.major > 2 else False
-if py3:
+from __future__ import print_function, unicode_literals
+
+import os
+import sys
+import signal
+import socket
+import subprocess
+import re
+import argparse
+from datetime import datetime
+
+VERSION = '0.0.4'
+
+PY3 = True if sys.version_info.major > 2 else False
+if PY3:
     import configparser
     customize_configparser = {'inline_comment_prefixes': ';'}
 else:
@@ -17,35 +24,38 @@ else:
     customize_configparser = {}
     FileNotFoundError = ConnectionRefusedError = socket.error
 
-if len(sys.argv) > 1 and (sys.argv[1] == '-v' or sys.argv[1] == '--verbose'):
-    verbose = True
-    print("lip v0.0.4")
-    from datetime import datetime
-else:
-    verbose = False
+
+def verbose_print(*args, **kwargs):
+    if not cli_args.verbose:
+        return
+    print(*args, **kwargs)
+
 
 def sigint_handler(signal, frame):
-    print("\n<Ctrl+C>")
+    print("\r<SIGINT>")
     lircd.close()
     sys.exit(0)
 
+
 def exec_cmd(cmd):   # cmd - str || list
-    if verbose: print("Key action: " + cmd)
+    verbose_print("Key action: " + cmd)
     if isinstance(cmd, str):
         cmd = cmd.split()
     if cmd[0] == 'run':
         cmd.pop(0)
     else:
         cmd.insert(0, 'xdotool')
-    subprocess.Popen(cmd)
+    subprocess.call(cmd)
+
 
 def get_cmd_out(cmd):   # cmd - str || list
     if isinstance(cmd, str):
         cmd = cmd.split()
     cmd_out = subprocess.check_output(cmd).rstrip()
-    if py3:
+    if PY3:
         cmd_out = cmd_out.decode('utf-8')
     return cmd_out
+
 
 comm_re = re.compile('([0-9A-Fa-f]+) ([0-9a-f]+) (.+) (.+)')
 xdo_cmd_version = ['xdotool', 'version']
@@ -53,15 +63,32 @@ xdo_cmd_windowname = ['xdotool', 'getactivewindow', 'getwindowname']
 lircd = socket.socket(socket.AF_UNIX)
 signal.signal(signal.SIGINT, sigint_handler)
 
+parser = argparse.ArgumentParser(description='lip')
+parser.add_argument('-c', '--config',
+                    required=False,
+                    help='config file')
+parser.add_argument('-v', '--verbose',
+                    action='store_true',
+                    help='verbose mode')
+cli_args = parser.parse_args()
+
+
+verbose_print("lip v" + VERSION)
+
 try:
     xdo_version = get_cmd_out(xdo_cmd_version)
 except (OSError, FileNotFoundError, subprocess.CalledProcessError):
     sys.exit("Can\'t execute xdotool")
-if verbose: print(xdo_version)
+verbose_print(xdo_version)
 
 config = configparser.ConfigParser(**customize_configparser)
-if not config.read(os.path.join(os.path.expanduser('~'), '.lip.ini')):
-    sys.exit("Can\'t open ~/.lip.ini")
+if cli_args.config:
+    config_path = os.path.abspath(cli_args.config)
+else:
+    config_path = os.path.join(os.path.expanduser('~'), '.liprc')
+if not config.read(config_path):
+    sys.exit("Can\'t open {}".format(config_path))
+verbose_print("config file: {}".format(config_path))
 try:
     config_rc_name = config.get('Settings', 'remote')
 except configparser.NoOptionError:
@@ -77,17 +104,19 @@ try:
 except (FileNotFoundError, ConnectionRefusedError):
     sys.exit("Can\'t open /dev/lircd")
 
+verbose_print("\n")
+
 while True:
     comm = lircd.recv(256).rstrip()
-    if py3:
+    if PY3:
         comm = comm.decode('utf-8')
-    if verbose: print("Time: {0:%X.%f %x}\nCommand: {1}".format(datetime.now(), comm))
+    verbose_print("Time: {0:%X.%f %x}\nCommand: {1}".format(datetime.now(), comm))
     comm_parsed = comm_re.match(comm)
     if comm_parsed:
         rc_key, rc_name = comm_parsed.group(3, 4)
         if not config_rc_name or rc_name == config_rc_name:
             active_window_name = get_cmd_out(xdo_cmd_windowname)
-            if verbose: print("Active window: " + active_window_name)
+            verbose_print("Active window: " + active_window_name)
             key_found = False
             for section in apps_sections:
                 if config.has_option(section, 'window_name') and re.search(config.get(section, 'window_name'), active_window_name):
@@ -100,4 +129,4 @@ while True:
                 exec_cmd(config.get('Default', rc_key))
     else:
         print("Can\'t parse: " + comm)
-    if verbose: print("\n")
+    verbose_print("\n")
